7b6862a787f8189c3b7e9986993f0c17020f813e
Leon <christl.leondaniel@gmail.com>
2026-02-08 14:40:46 +0100

    Created a better version of your chess

---
+++ Projects/Chess New and Improved/Chess V.2.c
@@ -0,0 +1,5 @@
+//
+// Created by Le-PC on 08/02/2026.
+//
+
+#include "Chess V.2.h"
---
+++ Projects/Chess New and Improved/Chess V.2.h
@@ -0,0 +1,8 @@
+//
+// Created by Le-PC on 08/02/2026.
+//
+
+#ifndef C_CHESS_V_2_H
+#define C_CHESS_V_2_H
+
+#endif //C_CHESS_V_2_H
