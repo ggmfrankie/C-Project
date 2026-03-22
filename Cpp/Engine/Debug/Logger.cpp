2e1cf4658d64f1e07141dd111fada6d8575e6a8b
Stefan <ertl.stefan2012@gmail.com>
2026-03-22 23:22:47 +0100

    created Logger.cpp

---
+++ Cpp/Engine/Debug/Logger.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Stefan on 22.03.2026.
+//
+
+#include "Logger.hpp"
+
+namespace Engine {
+} // Engine
---
+++ Cpp/Engine/Debug/Logger.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Stefan on 22.03.2026.
+//
+
+#pragma once
+
+namespace Engine {
+    class Logger {
+    };
+} // Engine
