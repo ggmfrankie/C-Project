8965a8016511c1ea79769d312a5f9cd2c9560def
Stefan <s.ertl@reinhausen.com>
2026-03-30 14:56:16 +0200

    started on implementing gameLayers

--- Cpp/Games/BaseGame/Layers/GameLayer.hpp
+++ Cpp/Games/BaseGame/Layers/GameLayer.hpp
@@ -3,9 +3,49 @@
 //
 
 #pragma once
+#include <string>
 
 
 namespace Game {
     class GameLayer {
+    private:
+        int age;
+        int ahhsa;
+    public:
+        GameLayer(int age) {
+            this->age = age;
+            ahhsa = 6;
+        }
     };
+
+    struct Human {
+    public:
+        std::string name;
+        int age;
+
+    public:
+        Human(std::string n) {
+            name = n;
+            age = 0;
+        }
+
+        void setAge(int a) {
+            age = a;
+        }
+    };
+
+
+
+    void test() {
+        GameLayer gm(7);
+
+        Human h{"Sepp"};
+
+        h.setAge(33);
+
+        Human* hp = new Human("haa");
+
+
+        delete hp;
+    }
 } // Game
