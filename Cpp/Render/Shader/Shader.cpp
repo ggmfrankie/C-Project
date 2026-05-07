c460956df8e07306b414d5260ddd66e12ea11f4a
Stefan <ertl.stefan2012@gmail.com>
2026-05-07 14:45:19 +0200

    Reworking Shader.cpp

--- Cpp/Render/Shader/Shader.cpp
+++ Cpp/Render/Shader/Shader.cpp
@@ -3,118 +3,194 @@
 //
 
 #include "Shader.hpp"
+#include "Shader.hpp"
 
-#include <iostream>
+#include <cassert>
+#include <cstring>
 #include <utility>
 
 #include "../../Utils/FileIO.hpp"
 #include "../../Utils/Math/Matrix.hpp"
 #include "../../Utils/Math/Vector.hpp"
 #include "glad/gl.h"
+#include "Utils/Utils.hpp"
+#include "Utils/DataStorage/LazyStream.hpp"
 
 namespace Render {
     using std::string;
-    Shader::Shader(string fileVert, string fileFrag):
-        vertPath(std::move(fileVert)),
-        fragPath(std::move(fileFrag))
-    {
-        programId = 0;
-        vertexId = 0;
-        fragmentId = 0;
-    }
+    using std::filesystem::path;
+    Shader::Shader(path&& fileVert, path&& fileFrag):
+        mVertPath(std::move(fileVert)),
+        mFragPath(std::move(fileFrag))
+    {}
 
     Shader::~Shader() {
-        glDeleteProgram(programId);
+        glDeleteProgram(mProgramId);
+    }
+
+    void Shader::typeCheck(UniformType a, UniformType b) {
+        return;
+        if (a == UniformType::Unknown || b == UniformType::Unknown) return;
+        if (a == b) return;
+        std::ostringstream error;
+        error << "[Warning] Uniform type '" << a << "' not matching with Uniform type '" << b << "' in shader program.\n";
+        throw ShaderException(error.str());
     }
 
     void Shader::setUniform(const string &name, const float value) const {
-        glUniform1f(getUniformLocation(name), value);
+        auto[loc, type] = getUniform(name);
+        typeCheck(type, UniformType::Float);
+        glUniform1f(loc, value);
     }
 
     void Shader::setUniform(const string &name, const int value) const {
-        glUniform1i(getUniformLocation(name), value);
+        auto[loc, type] = getUniform(name);
+        typeCheck(type, UniformType::Int);
+        glUniform1i(loc, value);
     }
 
     void Shader::setUniform(const string &name, const ggm::Vector3f& value) const {
-        glUniform3f(getUniformLocation(name), value.x, value.y, value.z);
+        auto[loc, type] = getUniform(name);
+        typeCheck(type, UniformType::Vec3);
+        glUniform3f(loc, value.x, value.y, value.z);
     }
 
     void Shader::setUniform(const string &name, const ggm::Matrix4f& value) const {
-        glUniformMatrix4fv(getUniformLocation(name),1, false, value.getDataPtr());
+        auto[loc, type] = getUniform(name);
+        typeCheck(type, UniformType::Mat4);
+        glUniformMatrix4fv(loc,1, false, value.getDataPtr());
+    }
+
+    void Shader::createUniform(const string &name, UniformType type) {
+        const GLint loc = glGetUniformLocation(mProgramId, name.c_str());
+        if (loc == -1) {
+            std::ostringstream error;
+            error << "[Warning] Uniform '" << name << "' not found in shader program.\n";
+            throw ShaderException(error.str());
+        }
+        mUniformLocationMap.try_emplace(name, Uniform{loc, type});
     }
 
     void Shader::createUniform(const string &name) {
-        const GLint loc = glGetUniformLocation(programId, name.c_str());
-        if (loc == -1) {
-            std::cerr << "[Warning] Uniform '" << name << "' not found in shader program.\n";
-        }
-        uniformLocationMap[name] = loc;
+       createUniform(name, UniformType::Unknown);
     }
 
-    GLint Shader::getUniformLocation(const std::string& name) const {
-        const auto it = uniformLocationMap.find(name);
-        if (it == uniformLocationMap.end()) {
-            std::cerr << "[Error] Uniform '" << name
-                      << "' was not created via Shader::createUniform.\n";
-            return -1;
+    const Shader::Uniform& Shader::getUniform(const std::string& name) const {
+        const auto it = mUniformLocationMap.find(name);
+        if (it == mUniformLocationMap.end()) {
+            std::ostringstream error;
+            error << "[Error] Uniform '" << name << "' was not created via Shader::createUniform.\n";
+            throw ShaderException(error.str());
         }
         return it->second;
     }
 
+    Shader::GLUniform::GLUniform(std::string_view type, std::string_view name) :
+        name(name),
+        type([](auto v) {
+            using namespace std::literals;
+            using std::pair;
+            constexpr std::array pairs{
+                pair{"int"sv, UniformType::Int},
+                pair{"float"sv, UniformType::Float},
+                pair{"bool"sv, UniformType::Bool},
+                pair{"vec2"sv, UniformType::Vec2},
+                pair{"vec3"sv, UniformType::Vec3},
+                pair{"vec4"sv, UniformType::Vec4},
+                pair{"mat3"sv, UniformType::Mat3},
+                pair{"mat4"sv, UniformType::Mat4},
+                pair{"sampler2D"sv, UniformType::Sampler2D},
+                pair{"samplerCube"sv, UniformType::SamplerCube}
+            };
+
+            for (auto[V, T]: pairs) {
+                if (v == V) return T;
+            }
+
+            return UniformType::Unknown;
+        }(type))
+    {}
+
+    std::vector<Shader::GLUniform> Shader::getUniforms(const std::string& shaderFile) {
+        using sv = const std::string_view;
+        auto out = ggm::LazyStream(ggm::split(shaderFile, '\n'))
+            .filter([](auto& a){ return a.starts_with("uniform ");})
+            .map([](auto a) {
+                a.remove_prefix(strlen("uniform "));
+                size_t pos = a.find_first_of(" ");
+                sv type = a.substr(0, pos);
+                sv name = a.substr(pos+1);
+
+                return GLUniform{type, name};
+            })
+            .toVector();
+
+        return out;
+    }
+
     void Shader::compile() {
-        programId = glCreateProgram();
+        mProgramId = glCreateProgram();
         int success;
         char infoLog[512];
 
-        vertexId = createVertexShader(vertPath);
-        fragmentId = createFragmentShader(fragPath);
+        const std::string vertShader = readShaderFile(mVertPath);
+        const std::string fragShader = readShaderFile(mFragPath);
 
-        glGetShaderiv(vertexId, GL_COMPILE_STATUS, &success);
+        mVertexId = createVertexShader(vertShader);
+        mFragmentId = createFragmentShader(fragShader);
+
+        glGetShaderiv(mVertexId, GL_COMPILE_STATUS, &success);
         if (!success) {
-            glGetShaderInfoLog(vertexId, 512, nullptr, infoLog);
-            printf("Vertex Shader Compile Error:\n%s\n", infoLog);
+            glGetShaderInfoLog(mVertexId, 511, nullptr, infoLog);
+            std::ostringstream error;
+            error << "Vertex Shader Compile Error:\n" << infoLog << "\n";
+            throw ShaderException(error.str());
         }
 
-        vertPath = {};
-        fragPath = {};
+        glGetShaderiv(mFragmentId, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(mFragmentId, 511, nullptr, infoLog);
+            std::ostringstream error;
+            error << "Fragment Shader Compile Error:\n" << infoLog << "\n";
+            throw ShaderException(error.str());
+        }
+
+        auto uniformsV = getUniforms(vertShader);
+        auto uniformsF = getUniforms(fragShader);
+
+        for (auto[name, type]: uniformsV) {
+            createUniform(string(name), type);
+        }
     }
 
     void Shader::link() const {
         int success;
         char infoLog[512];
-        glLinkProgram(programId);
+        glLinkProgram(mProgramId);
 
-        glGetProgramiv(programId, GL_LINK_STATUS, &success);
+        glGetProgramiv(mProgramId, GL_LINK_STATUS, &success);
         if (!success) {
-            glGetProgramInfoLog(programId, 512, nullptr, infoLog);
-            printf("Shader Program Link Error:\n%s\n", infoLog);
+            glGetProgramInfoLog(mProgramId, 512, nullptr, infoLog);
+            std::ostringstream error;
+            error << "Shader Program Link Error:\n" << infoLog << "\n";
+            throw ShaderException(error.str());
         }
-        glGetShaderiv(fragmentId, GL_COMPILE_STATUS, &success);
-        if (!success) {
-            glGetShaderInfoLog(fragmentId, 512, nullptr, infoLog);
-            printf("Fragment Shader Compile Error:\n%s\n", infoLog);
-        }
-        glValidateProgram(programId);
+
+        glValidateProgram(mProgramId);
     }
 
-    GLuint Shader::createVertexShader(const std::string &fileVert) const {
-        const std::string shaderSource = readShaderFile(fileVert);
-
+    GLuint Shader::createVertexShader(const std::string &shaderSource) const {
         const GLuint shaderId = createShader(shaderSource.c_str(), GL_VERTEX_SHADER);
-
         return shaderId;
     }
 
-    GLuint Shader::createFragmentShader(const std::string &fileFrag) const {
-        const std::string shaderSource = readShaderFile(fileFrag);
-
+    GLuint Shader::createFragmentShader(const std::string &shaderSource) const {
         const GLuint shaderId = createShader(shaderSource.c_str(), GL_FRAGMENT_SHADER);
-
         return shaderId;
     }
 
-    std::string Shader::readShaderFile(const std::string& fileName) {
-        return ggm::FileIO::readFile("../Cpp/Render/Shader/GlslShader/" + fileName);
+    std::string Shader::readShaderFile(const path& fileName) {
+        return ggm::FileIO::readFile("../Cpp/Render/Shader/GlslShader" / fileName);
     }
 
     GLuint Shader::createShader(const GLchar *code, const int shaderType) const {
@@ -125,19 +201,52 @@ namespace Render {
 
         glCompileShader(shaderId);
 
-        glAttachShader(programId, shaderId);
+        glAttachShader(mProgramId, shaderId);
         return shaderId;
     }
 
     void Shader::bind() const {
-        glUseProgram(programId);
+        glUseProgram(mProgramId);
     }
 
     void Shader::unbind() const {
         glUseProgram(0);
     }
 
+    Shader::ShaderException::ShaderException(const std::string &msg) :
+        runtime_error(msg)
+    {}
+
+    constexpr std::string_view
+    uniformTypeToString(Shader::UniformType type)
+    {
+        using enum Shader::UniformType;
+
+        switch (type) {
+            case Int:           return "int";
+            case Float:         return "float";
+            case Bool:          return "bool";
+
+            case Vec2:          return "vec2";
+            case Vec3:          return "vec3";
+            case Vec4:          return "vec4";
+
+            case Mat3:          return "mat3";
+            case Mat4:          return "mat4";
+
+            case Sampler2D:     return "sampler2D";
+            case SamplerCube:   return "samplerCube";
+
+            default:            return "unknown";
+        }
+    }
+
+    std::ostream& operator<<(std::ostream& lhs, Shader::UniformType rhs) {
+        lhs << uniformTypeToString(rhs);
+        return lhs;
+    }
 }
+
 
 
 
--- Cpp/Render/Shader/Shader.hpp
+++ Cpp/Render/Shader/Shader.hpp
@@ -2,9 +2,11 @@
 // Created by ertls on 17.02.2026.
 //
 
-#ifndef MIXEDPROJECT_SHADER_H
-#define MIXEDPROJECT_SHADER_H
+#pragma once
+
+#include <filesystem>
 #include <string>
+#include <vector>
 #include <unordered_map>
 
 #include "../../Utils/Math/Matrix.hpp"
@@ -13,13 +15,31 @@
 namespace Render {
     class Shader {
     public:
-        explicit Shader(std::string fileVert, std::string fileFrag);
+        explicit Shader(std::filesystem::path&& fileVert, std::filesystem::path&& fileFrag);
 
-        [[nodiscard]] GLuint createVertexShader(const std::string &fileVert) const;
+        [[nodiscard]] GLuint createVertexShader(const std::string &shaderSource) const;
 
-        static std::string readShaderFile(const std::string &fileName);
+        static std::string readShaderFile(const std::filesystem::path& fileName);
 
-        [[nodiscard]] GLuint createFragmentShader(const std::string &fileFrag) const;
+        [[nodiscard]] GLuint createFragmentShader(const std::string &shaderSource) const;
+
+        enum class UniformType {
+            Int,
+            Float,
+            Bool,
+
+            Vec2,
+            Vec3,
+            Vec4,
+
+            Mat3,
+            Mat4,
+
+            Sampler2D,
+            SamplerCube,
+
+            Unknown
+        };
 
         GLuint createShader(const GLchar *code, int shaderType) const;
 
@@ -28,27 +48,47 @@ namespace Render {
 
         virtual ~Shader();
 
-        void setUniform(const std::string &name, float value) const;
-        void setUniform(const std::string &name, int value) const;
-        void setUniform(const std::string &name, const  ggm::Vector3f& value) const;
-        void setUniform(const std::string &name, const ggm::Matrix4f &value) const;
+        static void typeCheck(UniformType a, UniformType b);
 
+        void setUniform(const std::string& name, float value) const;
+        void setUniform(const std::string& name, int value) const;
+        void setUniform(const std::string& name, const  ggm::Vector3f& value) const;
+        void setUniform(const std::string& name, const ggm::Matrix4f& value) const;
+
+        void createUniform(const std::string &name, UniformType type);
         void createUniform(const std::string &name);
-        GLint getUniformLocation(const std::string &name) const;
 
         virtual void compile();
         void link() const;
 
+        struct ShaderException : std::runtime_error{
+            explicit ShaderException(const std::string &msg);
+        };
+
     private:
-        std::string vertPath{};
-        std::string fragPath{};
-        std::unordered_map <std::string, GLint> uniformLocationMap{};
-        GLuint programId = 0;
-        GLuint vertexId = 0;
-        GLuint fragmentId = 0;
+        struct Uniform {
+            GLint location;
+            UniformType type;
+        };
 
+        std::filesystem::path mVertPath{};
+        std::filesystem::path mFragPath{};
+        std::unordered_map <std::string, Uniform> mUniformLocationMap{};
+        GLuint mProgramId = 0;
+        GLuint mVertexId = 0;
+        GLuint mFragmentId = 0;
+
+        struct GLUniform {
+            std::string_view name;
+            UniformType type;
+
+            GLUniform(std::string_view type, std::string_view name);
+        };
+
+        static std::vector<GLUniform>
+            getUniforms(const std::string& shaderFile);
+
+        const Uniform& getUniform(const std::string &name) const;
     };
+    std::ostream& operator<<(std::ostream& lhs, Shader::UniformType rhs);
 }
-
-
-#endif //MIXEDPROJECT_SHADER_H
