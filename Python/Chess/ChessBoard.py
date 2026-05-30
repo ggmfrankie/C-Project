b504ec402254b7b73e14566f93a18b84278124da
Stefan <ertl.stefan2012@gmail.com>
2026-05-30 18:06:47 +0200

    Some refactor

--- Python/Chess/ChessBoard.py
+++ Python/Chess/ChessBoard.py
@@ -1,11 +1,8 @@
 class ChessBoard:
-    test = 0
-
-    board: list[list[int]]
 
     def __init__(self, count):
         self.test = count
-        self.board = [[0] * 8 for _ in range(8)]
+        self.board: list[list[int]] = [[0] * 8 for _ in range(8)]
 
     def draw(self):
         pass
